daad748ee60ba722ef5bd70ec4df07d5196f4622
Dawid Lech <dawid.lech@pl.abb.com>
2021-07-25 16:59:49 +0200

    fixes

--- read_data.py
+++ read_data.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import collections
 import logging
 
-DWM = serial.Serial(port='COM8', baudrate=115200)
+DWM = serial.Serial(port='COM5', baudrate=115200)
 DWM.write("reset\r".encode())
 time.sleep(1)
 DWM.write('\r\r'.encode())
@@ -20,11 +20,16 @@ logging.getLogger("").addHandler(console)
 fig = plt.figure()
 plt.axis([0, 3, 0, 6])
 
-ShReg = collections.deque((), 3)
+p_cnt = 0
+
+# ShReg = collections.deque((), 3)
 
 while True:
     try:
         line = DWM.readline()
+        p_cnt += 1
+        if p_cnt % 3:
+            continue
         if line:
             parse = line.decode("utf-8").split(",")
             print(parse)
@@ -36,10 +41,10 @@ while True:
             now = datetime.now()
             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
             logging.info("Datetime: {:} | position: {:2.2f} {:2.2f} {:2.2f}".format(dt_string, x, y, z))
-            ShReg.append((x, y))
+            # ShReg.append((x, y))
             plt.axis([0, 3, 0, 6])
-            for (x, y) in ShReg:
-                plt.scatter(x, y)
+            # for (x, y) in ShReg:
+            plt.scatter(x, y)
             plt.pause(0.001)
             plt.clf()
     except Exception as e:
